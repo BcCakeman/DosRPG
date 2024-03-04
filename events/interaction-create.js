286244d3a979d5a640163ee8ece5d38ec99a8910
BcCakeman <bsumner2002@gmail.com>
2024-03-04 16:12:45 -0700

    fixed an issue that caused the bot to crash when you used optionSelect

--- events/interaction-create.js
+++ events/interaction-create.js
@@ -1,12 +1,14 @@
 import { Events } from 'discord.js';
 import { buildResponse } from '../utils/buildResponse.js';
-import games from '../utils/gamesLoader.js';
+import { games } from '../utils/gamesLoader.js';
 import { decode } from '../utils/saveEncoder.js';
 
 export default {
     name: Events.InteractionCreate,
     once: false,
     execute(interaction) {
+
+        console.log(`${interaction?.user.username} used interaction with id: ${interaction?.customId}`);
 
         //Interaction response for the optionSelector
         if (interaction?.isStringSelectMenu() && interaction?.customId === "optionSelect") {
@@ -16,13 +18,13 @@ export default {
 
             for (const game of games) {
                 if (oldSaveState.gameId === game.id) {
-                    
+
                     for (const scene of game.level) {
                         if (oldSaveState.currentScene === scene.id) {
-                            
+
                             const option = scene.options[interaction.values[0]];
                             newSaveState.currentScene = option[1];
-                            
+
                             interaction.message.delete();
                             interaction.reply(buildResponse(game, newSaveState));
                             return;
